c065a13ed9bca367f432900958e62f14c171cb57
karansharmaufl <sharma.karan9341@gmail.com>
2017-06-29 19:24:47 -0400

    Unstable

--- app/hyperloop/components/board.rb
+++ app/hyperloop/components/board.rb
@@ -1,51 +1,38 @@
   class Board < Hyperloop::Component
-
-    # param :my_param
-    # param param_with_default: "default value"
-    # param :param_with_default2, default: "default value" # alternative syntax
-    # param :param_with_type, type: Hash
-    # param :array_of_hashes, type: [Hash]
-    # collect_other_params_as :attributes  # collects all other params into a hash
-
-    # The following are the most common lifecycle call backs,
-    # the following are the most common lifecycle call backs# delete any that you are not using.
-    # call backs may also reference an instance method i.e. before_mount :my_method
-
     before_mount do
-      # any initialization particularly of state variables goes here.
-      # this will execute on server (prerendering) and client.
+      mutate.squares Array.new(9)
     end
 
-    after_mount do
-      # any client only post rendering initialization goes here.
-      # i.e. start timers, HTTP requests, and low level jquery operations etc.
+    def make_cell(i)
+      Button(value: state.squares[i]).on(:on_handle_click)do
+      puts "*****Clicked****"
+        click_event(i)
+      end
     end
 
-    before_update do
-      # called whenever a component will be re-rerendered
-    end
-
-    before_unmount do
-      # cleanup any thing (i.e. timers) before component is destroyed
+    def click_event(i)
+      squares = state.squares.clone
+      squares[i] = 'X'
+      mutate.squares squares
     end
 
     def render
       DIV do
         H1 do "Next Turn, Player: X" end
         DIV(class: 'row') do
-          Button(temp: "1")
-          Button(temp: "2")
-          Button(temp: "3")
+          make_cell(1)
+          make_cell(2)
+          make_cell(3)
         end
         DIV(class: 'row') do
-          Button(temp: "4")
-          Button(temp: "5")
-          Button(temp: "6")
+          make_cell(4)
+          make_cell(5)
+          make_cell(6)
         end
         DIV(class: 'row') do
-          Button(temp: "7")
-          Button(temp: "8")
-          Button(temp: "9")
+          make_cell(7)
+          make_cell(8)
+          make_cell(9)
         end
 
       end
--- app/hyperloop/components/button.rb
+++ app/hyperloop/components/button.rb
@@ -1,45 +1,13 @@
-
   class Button < Hyperloop::Component
-
-    # param :my_param
-    # param param_with_default: "default value"
-    # param :param_with_default2, default: "default value" # alternative syntax
-    # param :param_with_type, type: Hash
-    # param :array_of_hashes, type: [Hash]
-    # collect_other_params_as :attributes  # collects all other params into a hash
-
-    # The following are the most common lifecycle call backs,
-    # the following are the most common lifecycle call backs# delete any that you are not using.
-    # call backs may also reference an instance method i.e. before_mount :my_method
-
-
-    param :temp , type: String
-
-    before_mount do
-      # any initialization particularly of state variables goes here.
-      # this will execute on server (prerendering) and client.
-      mutate.temp nil
-    end
-
-    after_mount do
-      # any client only post rendering initialization goes here.
-      # i.e. start timers, HTTP requests, and low level jquery operations etc.
-    end
-
-    before_update do
-      # called whenever a component will be re-rerendered
-    end
-
-    before_unmount do
-      # cleanup any thing (i.e. timers) before component is destroyed
-    end
+    param :on_handle_click , type: Proc
+    param :value 
 
     def render
       DIV do
         BUTTON(class: 'btn btn-lg')do
-          state.temp
-        end.on(:click) do
-            mutate.temp "X"
+          params.value
+            end.on(:click) do
+              params.on_handle_click
         end
       end
     end
