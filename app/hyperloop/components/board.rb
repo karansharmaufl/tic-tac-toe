c89b00ab11ad873febca0365c07e264cf64d8945
karansharmaufl <sharma.karan9341@gmail.com>
2017-06-30 00:29:44 -0400

    Check winner added

--- app/hyperloop/components/board.rb
+++ app/hyperloop/components/board.rb
@@ -1,40 +1,86 @@
   class Board < Hyperloop::Component
+
+    #param :message_turn type: String
     before_mount do
       mutate.squares Array.new(9)
+      mutate.next_turn TRUE
     end
 
     def make_cell(i)
-      Button(value: state.squares[i]).on(:on_handle_click)do
-      puts "*****Clicked****"
+      Button(value: state.squares[i]).on(:handle_click)do
         click_event(i)
       end
     end
 
     def click_event(i)
       squares = state.squares.clone
-      squares[i] = 'X'
+      if state.next_turn
+        squares[i]='X'
+      else
+        squares[i]='O'
+      end
       mutate.squares squares
+      mutate.next_turn !state.next_turn
+
     end
 
-    def render
-      DIV do
-        H1 do "Next Turn, Player: X" end
-        DIV(class: 'row') do
-          make_cell(1)
-          make_cell(2)
-          make_cell(3)
-        end
-        DIV(class: 'row') do
-          make_cell(4)
-          make_cell(5)
-          make_cell(6)
-        end
-        DIV(class: 'row') do
-          make_cell(7)
-          make_cell(8)
-          make_cell(9)
-        end
+    def wins
+     [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [2,4,6]
+     ]
+    end
 
+    def calculate_winner
+      winner = nil
+      wins.each do |win|
+        a=win[0]
+        b=win[1]
+        c=win[2]
+        if state.squares[a] and state.squares[a]==state.squares[b] and state.squares[a]==state.squares[c]
+          winner = state.squares[a]
+        end
+      end
+      return winner   
+    end
+    
+    
+  
+    def render
+      winner = calculate_winner
+      message = ""      
+      if winner
+        message = "Winner: #{winner}"
+      else
+        if state.next_turn
+          message = "Current turn: X"
+        else
+          message = "Current turn: O"
+        end
+      end
+      DIV do
+        H1 do "#{ message }" end
+          DIV(class: 'row') do
+            make_cell(0)
+            make_cell(1)
+            make_cell(2)
+          end
+          DIV(class: 'row') do
+            make_cell(3)
+            make_cell(4)
+            make_cell(5)
+          end
+          DIV(class: 'row') do
+            make_cell(6)
+            make_cell(7)
+            make_cell(8)
+          end
       end
     end
   end
