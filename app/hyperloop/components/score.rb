e542c254ff4565d57ff3cbb6eceee823a831f12d
karansharmaufl <sharma.karan9341@gmail.com>
2017-07-12 18:20:24 -0400

    Unstable

---
+++ app/hyperloop/components/score.rb
@@ -0,0 +1,11 @@
+class Score < Hyperloop::Component
+    param :x_score
+    param :y_score
+    def render
+      div do
+        H2 do "X-Score: #{params.x_score}" end
+        H2 do "O-Score: #{params.y_score}" end
+      end
+    end
+  end
+
